4f32be4c2931d4ca20dd3bf95aa9a24fede5d402
mthomas7503 <97054832+mthomas7503@users.noreply.github.com>
2022-12-03 12:17:14 -0500

    commit tree d2d4d4fd5e5a93e3ad7f8384df525d2c4f76ddd5

---
+++ lib/song.rb
@@ -0,0 +1,40 @@
+require 'pry'
+
+class Song
+
+    attr_accessor :genre, :name, :artist
+
+    @@count = 0
+    @@artists = []
+    @@genres = []
+
+    def initialize (name, artist, genre)
+        @name = name
+        @artist = artist
+        @genre = genre
+
+        @@genres << genre
+        @@artists << artist
+        @@count += 1
+    end
+
+    def self.count
+        @@count
+    end
+
+    def self.genres
+        @@genres.uniq
+    end
+
+    def self.artists
+        @@artists.uniq
+    end
+
+    def self.genre_count
+        @@genres.tally
+    end
+
+    def self.artist_count
+        @@artists.tally
+    end
+end
